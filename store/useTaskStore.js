e408d5392a6ea61612a8b492a53eb64d445bd15f
watiprojects <wati.projects01@gmail.com>
2025-08-23 16:01:05 +0200

    add state to zustand store

--- store/useTaskStore.js
+++ store/useTaskStore.js
@@ -1,4 +1,15 @@
+import { create } from 'zustand';
+
 const useTaskStore = create((set, get) => ({
+/*-----------------
+STATE
+-----------------*/
+//the layout is objects with key value pairs
+
+tasks: [], //empty array of tasks which will have {id, text, and iuvalue (ie. importance/urgency value)}
+currentStep: 1, // 1: Mind sweep, 2: Review & Categorize, 3: View Results
+currentTaskIndex: 0, //index for Step 2 navigation
+
 })
 
 );
