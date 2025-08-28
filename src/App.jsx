5201d9642ebf0e702dcfe9163f3f21118257c0e2
watiprojects <wati.projects01@gmail.com>
2025-08-28 12:03:40 +0200

    testing list and form placement

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,25 @@ import './App.css'
 function App() {
   
   return (
-    <>
+    <div className="flex-col items-center">
       <h1>The EISENHOWer</h1>
       <h2>Step 1: Mind Sweep</h2>
       <p>What tasks are swirling around in your mind? List them all!</p>
-      <div>
-        <button type="submit" className="px-4 py-2 bg-gray-600 text-white rounded-lg">Submit</button>
+      <div className="flex-col text-left">
+        <div className="mind-sweep-div bg-gray-200 h-[40vh] p-2 m-4 rounded-lg">
+          <ul className="list-disc list-inside">
+            <li>
+              <input
+                type ="text"
+                placeholder = "Insert item here"
+              />
+            </li>
+            <li>test</li>
+          </ul>
+        </div>
       </div>
-    </>
+      <button type="submit" className="px-4 py-2 bg-gray-600 text-white rounded-lg">Submit</button>
+    </div>
   )
 }
 
