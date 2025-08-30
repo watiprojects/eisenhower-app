b82e9e1451dd75063a8102e12135bf55673d0f4b
watiprojects <wati.projects01@gmail.com>
2025-08-30 22:57:10 +0200

    update task card

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,15 +1,34 @@
 import { useTaskStore } from '../../store/useTaskStore'
+import { useEffect } from 'react';
 
 
 function TaskCard ({task}){
-    const {tasks, currentTaskIndex} = useTaskStore();
+    const {tasks, currentTaskIndex, toggleImportant, toggleUrgent} = useTaskStore();
+    const currentTask = tasks[currentTaskIndex];
     
     //statement if no tasks
     if (tasks.length === 0) {
         return <p> No tasks yet </p>;
     }
-    
-    const currentTask = tasks[currentTaskIndex]
+
+    //handle keyboard navigation
+        useEffect(() => {
+            const handleKeyDown = (e) => {
+                //go to next task if arrow right
+                if (e.key === "i") {
+                    toggleImportant();
+                }
+                //go to previous task if arrow right
+                else if (e.key === "u") {
+                    toggleUrgent();
+                }
+            }
+            window.addEventListener("keydown", handleKeyDown);
+            return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+            };
+        }, [toggleImportant, toggleUrgent]);
+
 
     return (   
         <div className="m-6 p-4 w-sm border rounded shadow">
@@ -19,6 +38,11 @@ function TaskCard ({task}){
             <p className="text-sm text-gray-500">
                 Task {currentTaskIndex + 1} of {tasks.length} 
             </p>
+            {/* display important and urgent labels*/}
+            {/* if important is active display in box
+                if it is true that the current task has important then display the words important at the bottom
+            */}
+            <p></p>
         </div>
         
     );
