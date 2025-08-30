a4201e11750e8cbb7ca519d45725d5d611e8cfe2
watiprojects <wati.projects01@gmail.com>
2025-08-30 15:33:51 +0200

    update step 2 and 3 pages

--- src/pages/StepThree.jsx
+++ src/pages/StepThree.jsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function StepThree() {
+    const navigate = useNavigate(); // hook to the navigate function
 
+        return (
+        <div className="flex-col items-center">
+        <h1>Here is your Eisenhower Matrix!</h1>
+        <div>
+        </div>
+            <button type="button" className="px-4 py-2 bg-gray-600 text-white rounded-lg" 
+            onClick={() => {navigate("/step2")
+            }}>Back</button>
+    </div>
+    )
     }
 
 export default StepThree;
--- src/pages/StepTwo.jsx
+++ src/pages/StepTwo.jsx
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function StepTwo() {
+    
+    const navigate = useNavigate(); // hook to the navigate function
 
+        return (
+        <div className="flex-col items-center">
+        <h1>The EISENHOWer</h1>
+        <h2>Step 2: Review and categorize</h2>
+        <p>Is this important and/or urgent?</p>
+        <div>
+        </div>
+            <button type="button" className="px-4 py-2 bg-gray-600 text-white rounded-lg" 
+            onClick={() => {navigate("/step3")
+            }}>Submit</button>
+    </div>
+    )
     }
 
 export default StepTwo;
