4754333a056cc4385e92afd686070fc70340cba3
watiprojects <wati.projects01@gmail.com>
2025-08-31 06:57:55 +0200

    css grid

--- src/pages/StepThree.jsx
+++ src/pages/StepThree.jsx
@@ -6,47 +6,58 @@ function StepThree() {
     const {tasks, addTask, editTask} = useTaskStore();
 
         return (
-        <div className="flex-col items-center">
-        <h1>Here is your Eisenhower Matrix!</h1>
-        <div>
-            <p className="font-bold">Quadrant 1: Important and Urgent</p>
-            <ul className="list-disc list-inside">
-                {/*Quadrant 1: List task if task is urgent and important*/}
-                {tasks.filter(task => task.important && task.urgent).map(task => (
-                    <li key = {task.id}>{task.text}</li>
-                ))}
-            </ul>
+        <div className="flex flex-col items-center">
+            <h1>Here is your Eisenhower Matrix!</h1>
+            {/*Eisenhower Matrix div*/}
+            <div>
+                <div className="grid grid-cols-2 gap-4 mb-4">
+                    <div className="bg-gray-300 p-4 rounded">
+                        <p className="font-bold">Quadrant 1: Important and Urgent</p>
+                        <ul className="list-disc list-inside ">
+                            {/*Quadrant 1: List task if task is urgent and important*/}
+                            {tasks.filter(task => task.important && task.urgent).map(task => (
+                                <li key = {task.id}>{task.text}</li>
+                            ))}
+                        </ul>
+                    </div>
 
-            <p className="font-bold">Quadrant 2: Important, Not Urgent</p>
-            <ul className="list-disc list-inside">
-                {/*Quadrant 2: Important, not urgent*/}
-                {tasks.filter(task => task.important && !task.urgent).map(task => (
-                    <li key = {task.id}>{task.text}</li>
-                ))}
-            </ul>
+                    <div className="bg-gray-300 p-4 rounded">
+                        <p className="font-bold">Quadrant 2: Important, Not Urgent</p>
+                        <ul className="list-disc list-inside">
+                            {/*Quadrant 2: Important, not urgent*/}
+                            {tasks.filter(task => task.important && !task.urgent).map(task => (
+                                <li key = {task.id}>{task.text}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 gap-4 mb-4">
+                    <div className="bg-gray-300 p-4 rounded">
+                        <p className="font-bold">Quadrant 3: Urgent, Not Important </p>
+                        <ul className="list-disc list-inside">
+                            {/*Quadrant 3: Urgent, Not Important*/}
+                            {tasks.filter(task => !task.important && task.urgent).map(task => (
+                                <li key = {task.id}>{task.text}</li>
+                            ))}
+                        </ul>
+                    </div>
 
-            <p className="font-bold">Quadrant 3: Urgent, Not Important </p>
-            <ul className="list-disc list-inside">
-                {/*Quadrant 3: Urgent, Not Important*/}
-                {tasks.filter(task => !task.important && task.urgent).map(task => (
-                    <li key = {task.id}>{task.text}</li>
-                ))}
-            </ul>
+                    <div className="bg-gray-300 p-4 rounded">
+                        <p className="font-bold">Quadrant 4: Not Important, Not Urgent </p>
+                        <ul className="list-disc list-inside">
+                            {/*Quadrant 4: Not Important, Not Urgent*/}
+                            {tasks.filter(task => !task.important && !task.urgent).map(task => (
+                                <li key = {task.id}>{task.text}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-            <p className="font-bold">Quadrant 4: Not Important, Not Urgent </p>
-            <ul className="list-disc list-inside">
-                {/*Quadrant 4: Not Important, Not Urgent*/}
-                {tasks.filter(task => !task.important && !task.urgent).map(task => (
-                    <li key = {task.id}>{task.text}</li>
-                ))}
-            </ul>
-            
-
-        </div>
             <button type="button" className="px-4 py-2 bg-gray-600 text-white rounded-lg" 
             onClick={() => {navigate("/step2")
             }}>Back</button>
-    </div>
+        </div>
     )
     }
 
